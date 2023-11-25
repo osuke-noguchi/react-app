86bc21a5fd46082f324db3a54a2f207c01b0e11d
osuke-noguchi <noguchi.nodai@gmail.com>
2023-11-26 07:13:53 +0900

    ♻️ メイン部分のコンポーネント化

---
+++ components/Main.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import { Links } from './Links';
+import { Headline } from './Headline';
+
+export function Main(props) {
+  return (
+    <main className={styles.main}>
+      <Headline page={props.page}>
+        <code className={styles.code}>pages/{props.page}.js</code>
+      </Headline>
+      <Links />
+    </main>
+  );
+}
