1004a7ca6fc0bceff2e2fd7d912e7bdd177af654
osuke-noguchi <noguchi.nodai@gmail.com>
2023-11-29 05:44:36 +0900

    ✨ useEffectの導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,14 +3,34 @@ import styles from 'src/styles/Home.module.css';
 import { Footer } from 'src/components/Footer';
 import { Main } from 'src/components/Main';
 import { Header } from 'src/components/Header';
+import { useEffect } from 'react';
+
+const handleClick = (e) => {
+    e.preventDefault();
+  };
 
 export default function Home() {
+
+  useEffect(() => {
+    console.log("マウント時の処理");
+    document.body.style.backgroundColor = "lightblue";
+
+    return () => {
+      console.log("アンマウント時の処理");
+      document.body.style.backgroundColor = "";
+    }
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
+      <a
+        href="/about"
+        onClick={handleClick}
+      >ボタン</a>
       <Main page="index"></Main>
       <Footer />
     </div>
