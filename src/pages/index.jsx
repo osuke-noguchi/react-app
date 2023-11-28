b54126af0dd8c7078611e3257f6f8dcbdba40b03
osuke-noguchi <noguchi.nodai@gmail.com>
2023-11-29 06:28:18 +0900

    ⚡️ useCallback、useEffectの第2引数を使用した実装

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,23 +3,26 @@ import styles from 'src/styles/Home.module.css';
 import { Footer } from 'src/components/Footer';
 import { Main } from 'src/components/Main';
 import { Header } from 'src/components/Header';
-import { useEffect, useState } from 'react';
-
+import { useCallback, useEffect, useState } from 'react';
 
 export default function Home() {
   const [count, setCount] = useState(1);
 
-  const handleClick = () => {
-    setCount((count) => count  + 1);
-    setCount((count) => count  + 1);
-  };
+  const handleClick = useCallback(
+    (e) => {
+      if (count < 10) {
+        setCount((count) => count + 1);
+      }
+    },
+    [count]
+  );
 
   useEffect(() => {
-    document.body.style.backgroundColor = "lightblue";
+    document.body.style.backgroundColor = 'lightblue';
 
     return () => {
-      document.body.style.backgroundColor = "";
-    }
+      document.body.style.backgroundColor = '';
+    };
   }, []);
 
   return (
@@ -29,8 +32,7 @@ export default function Home() {
       </Head>
       <Header />
       <h1>{count}</h1>
-      <button onClick={handleClick}
-      >ボタン</button>
+      <button onClick={handleClick}>ボタン</button>
       <Main page="index"></Main>
       <Footer />
     </div>
