01659fdd32bf272acfa7e3cf1f5adab16afbed05
osuke-noguchi <noguchi.nodai@gmail.com>
2023-11-30 05:44:16 +0900

    ✨ useStateで文字列と真偽値を実装

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,6 +7,8 @@ import { useCallback, useEffect, useState } from 'react';
 
 export default function Home() {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState('');
+  const [isShow, setIsShow] = useState(true);
 
   const handleClick = useCallback(
     (e) => {
@@ -16,6 +18,14 @@ export default function Home() {
     },
     [count]
   );
+
+  const onChange = useCallback((e) => {
+    setText(e.target.value.trim());
+  }, []);
+
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow);
+  }, []);
 
   useEffect(() => {
     document.body.style.backgroundColor = 'lightblue';
@@ -31,8 +41,10 @@ export default function Home() {
         <title>Index Page</title>
       </Head>
       <Header />
-      <h1>{count}</h1>
+      {isShow ? <h1>{count}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
+      <button onClick={handleDisplay}>{isShow ? '非表示' : '表示'}</button>
+      <input type="text" value={text} onChange={onChange} />
       <Main page="index"></Main>
       <Footer />
     </div>
