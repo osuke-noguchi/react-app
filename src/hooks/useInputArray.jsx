6d60a56a315fdd32179300553850e803afad1d4b
osuke-noguchi <noguchi.nodai@gmail.com>
2023-12-01 05:30:20 +0900

    🎨 カスタムフックスを使用した実装に変更

---
+++ src/hooks/useInputArray.jsx
@@ -0,0 +1,22 @@
+import { useCallback, useState } from 'react';
+
+export const useInputArray = () => {
+  const [text, setText] = useState('');
+  const [array, setArray] = useState([]);
+
+  const handleAdd = useCallback(() => {
+    setArray((prevArray) => {
+      if (prevArray.some((item) => item === text)) {
+        alert('同じ値がすでに存在します');
+        return prevArray;
+      }
+      return [...prevArray, text];
+    });
+  }, [text]);
+
+  const onChange = useCallback((e) => {
+    setText(e.target.value.trim());
+  }, []);
+
+  return { text, array, handleAdd, onChange };
+};
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,48 +3,14 @@ import styles from 'src/styles/Home.module.css';
 import { Footer } from 'src/components/Footer';
 import { Main } from 'src/components/Main';
 import { Header } from 'src/components/Header';
-import { useCallback, useEffect, useState } from 'react';
+import { useCounter } from 'src/hooks/useCounter';
+import { useInputArray } from 'src/hooks/useInputArray';
+import { useBgLightBlue } from 'src/hooks/useBgLightBlue';
 
 export default function Home() {
-  const [count, setCount] = useState(1);
-  const [text, setText] = useState('');
-  const [isShow, setIsShow] = useState(true);
-  const [array, setArray] = useState([]);
-
-  const handleClick = useCallback(
-    (e) => {
-      if (count < 10) {
-        setCount((prevCount) => prevCount + 1);
-      }
-    },
-    [count]
-  );
-
-  const handleDisplay = useCallback(() => {
-    setIsShow((prevIsShow) => !prevIsShow);
-  }, []);
-
-  const handleAdd = useCallback(() => {
-    setArray((prevArray) => {
-      if (prevArray.some(item => item === text)) {
-        alert("同じ値がすでに存在します");
-        return prevArray;
-      }
-      return[...prevArray, text];
-    });
-  }, [text]);
-
-  const onChange = useCallback((e) => {
-    setText(e.target.value.trim());
-  }, []);
-
-  useEffect(() => {
-    document.body.style.backgroundColor = 'lightblue';
-
-    return () => {
-      document.body.style.backgroundColor = '';
-    };
-  }, []);
+  const { count, isShow, handleClick, handleDisplay } = useCounter();
+  const { text, array, handleAdd, onChange } = useInputArray();
+  useBgLightBlue();
 
   return (
     <div className={styles.container}>
