a2338ddccea4faddba2787026eb8a3e374376186
osuke-noguchi <noguchi.nodai@gmail.com>
2023-12-01 06:08:08 +0900

    ✨ コンポーネント間のリフトアップ

--- src/components/Headline/index.jsx
+++ src/components/Headline/index.jsx
@@ -4,10 +4,8 @@ export function Headline(props) {
   return (
     <div>
       <h1 className={styles.title}>{props.page} page</h1>
-
-      <p className={styles.description}>
-        Get started by editing {props.children}
-      </p>
+      <p className={styles.description}>アイテムの数は {props.children}</p>
+      <button onClick={props.handleReduce}>減らす</button>
     </div>
   );
 }
